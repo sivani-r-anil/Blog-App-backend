27b237fc8a11629b99ba275ce925222c5172e465
sivani-r-anil <sivaniranil28@gmail.com>
2026-07-16 00:17:35 +0530

    SignIn api created

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.use(cors())               //security purpose
 
 mongoose.connect("mongodb://sivani_r_anil:siva123@ac-0k7dbum-shard-00-00.ojotgu3.mongodb.net:27017,ac-0k7dbum-shard-00-01.ojotgu3.mongodb.net:27017,ac-0k7dbum-shard-00-02.ojotgu3.mongodb.net:27017/blogdb?ssl=true&replicaSet=atlas-gxc29u-shard-0&authSource=admin&appName=Cluster0")
 
+
+
+//signUp api
 app.post("/signup", async (req, res) => {
 
     let input = req.body
@@ -43,6 +46,41 @@ app.post("/signup", async (req, res) => {
 })
 
 
+
+//signIn api
+app.post("/signin", async (req, res) => {
+
+    let input = req.body
+    let result = userModel.find({ email: req.body.email }).then(
+
+        (items) => {
+            if (items.length > 0) {
+                const passwordValidator= bcrypt.compareSync(req.body.password, items[0].password)
+                if(passwordValidator){
+
+                    jwt.sign({ email: req.body.email }, "blogApp", {expiresIn: "1d"}, (err, token) => {
+                        if(err) {
+                            res.json({ "status": "error", "error": err })
+                        }
+                        else {
+                            res.json({ "status": "Success", "token": token ,"user": items[0]._id})           //token is generated and sent to the user.token is used to verify the user and to check if the user is logged in or not. it is used for security purpose.
+                        }
+
+                    })
+                }
+                else{
+                    res.json({ "status": "invalid password" })
+                }
+            }
+            else
+            {
+                res.json({ "status": "invalid email" })
+            }
+        }
+
+    ).catch()
+
+})
 
 
 app.listen(2000, () => {
